92079492517fdf1e26f28335f356595f282e5eb2
Eiichi Yoshikawa <edo@bari-ikutsu.com>
2024-03-22 13:00:45 +0900

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bari_ikutsu.lnautoanswer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
